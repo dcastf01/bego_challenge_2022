4e6f49fb13cdd13709d06c62174902f3d8958f5f
dcastf <dcastf01@gmail.com>
2022-07-02 13:20:05 +0200

    template with few modifcations

---
+++ streamlit_app.py
@@ -0,0 +1,63 @@
+import pdfkit
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+from datetime import date
+import streamlit as st
+from streamlit.components.v1 import iframe
+import pandas as pd
+import numpy as np
+st.set_page_config(layout="centered", page_icon="🎓", page_title="Diploma Generator")
+st.title("🎓 Diploma PDF Generator")
+
+st.write(
+    "This app is a challenge for my girlfriend to get her birthday present"
+)
+
+left, right = st.columns(2)
+
+
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+right.write("Here's the template we'll be using:")
+right.selectbox('Elige la plantilla correcta para resolver el challenge',['a','b','c','d'])
+right.dataframe(df)
+
+env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
+template = env.get_template("template.html")
+
+
+left.write("Fill in the data:")
+form = left.form("template_form")
+winner = form.text_input("Winner name")
+# course = form.selectbox(
+#     "Choose course",
+#     ["Report Generation in Streamlit", "Advanced Cryptography"],
+#     index=0,
+# )
+# grade = form.slider("Grade", 1, 100, 60)
+submit = form.form_submit_button("Generate PDF")
+
+# submit=True
+
+# winner='BEGO'
+
+## we generate the certificate
+if submit:
+    html = template.render(
+        winner=winner,
+        date=date.today().strftime("%B %d, %Y"),
+    )
+    path_wkhtmltopdf = r'D:\programacion\Repositorios\bego_challenge_2022\whkhtml\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf,)
+    pdf = pdfkit.from_string(html, False,configuration=config)
+    st.balloons()
+
+    right.success("🎉 Your diploma was generated!")
+    # st.write(html, unsafe_allow_html=True)
+    # st.write("")
+    right.download_button(
+        "⬇️ Download PDF",
+        data=pdf,
+        file_name="diploma.pdf",
+        mime="application/octet-stream",
+    )
